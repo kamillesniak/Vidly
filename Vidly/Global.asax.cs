86530e7b3a8e44b938962f8dcd8d5c692dd12655
kamillesniak <kamillesniak02@gmail.com>
2018-11-24 20:40:53 +0100

    WebApi for Customer

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,6 +15,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
